f3ffc24db6bdba0fc8b97829b89ea6946f3fb117
ALaa Khaled <alaa.khaled_a108@fci.kfs.edu.eg>
2026-05-22 14:13:44 +0300

    Remove unused lines

--- static/src/js/patches/PosOrderPatch.js
+++ static/src/js/patches/PosOrderPatch.js
@@ -6,8 +6,6 @@ import { patch } from "@web/core/utils/patch";
 patch(PosOrder.prototype, {
   setup(vals) {
     super.setup(...arguments);
-    // uiState doesn't exist yet during creation (initState runs after setup in Odoo 19).
-    // Cache the value for initState; for updates uiState already exists so set directly.
     this._pendingTagIds = vals.tag_ids || [];
     if (this.uiState) {
       this.uiState.digizilla_tag_ids = this._pendingTagIds;
